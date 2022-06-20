fada3ac12498072669408bd04336c57d64e01d01
Ben Parsons <9parsonsb@gmail.com>
2022-06-20 14:45:41 +1000

    Add POC for capture button detection

---
+++ UsbPcapLib.TestHarness/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using USBPcapLib;
+using UsbPcapLib.TestHarness;
+
+var filters = USBPcapClient.find_usbpcap_filters();
+
+Console.WriteLine(filters.Aggregate((a,b) => a + Environment.NewLine + USBPcapClient.enumerate_print_usbpcap_interactive(b)));
+
+var devices = filters.Select(f =>(filterId:f, deviceTree: USBPcapClient.enumerate_print_usbpcap_interactive(f)))
+    .OrderByDescending(f => f.deviceTree.Length);
+
+var primary = devices.First();
+
+foreach (var filter in filters)
+{
+    Console.WriteLine($"Filter: {filter}: ");
+    Console.WriteLine();
+}
+
+Console.WriteLine($"Choosing Filter: {primary.filterId} ");
+
+var client = new USBPcapClient(primary.filterId);
+
+client.DataRead += (sender, eventArgs) =>
+{
+    if (eventArgs.Data.Length > 100)
+    {
+  //      Console.WriteLine($"DATA READ Device:'{eventArgs.Header.device}' in?:{eventArgs.Header.In} func:'{eventArgs.Header.function}' len: {eventArgs.Data.Length} ");
+    }
+    else
+    {
+//        Console.WriteLine($"DATA READ Device:'{eventArgs.Header.device}' in?:{eventArgs.Header.In} func:'{eventArgs.Header.function}' len: {eventArgs.Data.Length} data:{eventArgs.Data.ToHexString()}");
+    }
+
+    if (eventArgs.Header.In && eventArgs.Data.Length > 0 && Utils.CompareBytes(eventArgs.Data, new byte[] {0x02, 0x01, 0x00, 0x00}, 4))
+    {
+        Console.WriteLine("CAPTURE!!!!");
+    }
+};
+
+client.start_capture();
+
+client.wait_for_exit_signal();
---
+++ UsbPcapLib.TestHarness/Utils.cs
@@ -0,0 +1,32 @@
+﻿namespace UsbPcapLib.TestHarness;
+
+public static class Utils
+{
+    /// <summary>
+    /// Convert byte array to string as a sequence of hex values.
+    /// </summary>
+    /// <param name="bytes"></param>
+    /// <returns></returns>
+    public static string ToHexString(this IEnumerable<byte> bytes, string deliminator = "")
+    {
+        return bytes.Aggregate(string.Empty, (current, b) => $"{current}{b:x2}{deliminator}")[..^deliminator.Length];
+    }
+    
+    /// <summary>
+    /// Compare two bytes array
+    /// </summary>
+    /// <param name="a1"></param>
+    /// <param name="a2"></param>
+    /// <param name="length"></param>
+    /// <returns>bool</returns>
+    public static bool CompareBytes(ReadOnlySpan<byte> a1, ReadOnlySpan<byte> a2, int length)
+    {
+        var isEqual = true;
+        for (var i = 0; i < length && i < a1.Length && i < a2.Length; i++)
+        {
+            isEqual = isEqual && a1[i] == a2[i];
+        }
+
+        return isEqual;
+    }
+}
