d754417a48afb157d8dd55fae4710750ee6e3bea
Ben Parsons <9parsonsb@gmail.com>
2022-07-01 13:51:00 +1000

    update test code

--- UsbPcapDotNet.TestHarness/Program.cs
+++ UsbPcapDotNet.TestHarness/Program.cs
@@ -1,33 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.InteropServices;
 using UsbPcapDotNet;
 using UsbPcapDotNet.TestHarness;
 
 var filters = USBPcapClient.find_usbpcap_filters();
 
-Console.WriteLine(
-    filters.Aggregate((a, b) => a + Environment.NewLine + USBPcapClient.enumerate_print_usbpcap_interactive(b)));
+// Console.WriteLine(
+//     filters.Aggregate((a, b) => a + Environment.NewLine + USBPcapClient.enumerate_print_usbpcap_interactive(b)));
 
 var devices = filters.Select(f => (filterId: f, deviceTree: USBPcapClient.enumerate_print_usbpcap_interactive(f)))
-   .OrderByDescending(f => f.deviceTree.Length);
+   .OrderByDescending(f => f.deviceTree.Length).ToList();
 
 var primary = devices.First();
 
-foreach (var filter in filters)
+foreach (var filter in devices)
 {
-    Console.WriteLine($"Filter: {filter}: ");
-    Console.WriteLine();
+    Console.WriteLine($"Filter: {filter.filterId}: ");
+    Console.WriteLine(filter.deviceTree);
 }
 
 Console.WriteLine($"Choosing Filter: {primary.filterId} ");
-
 
 var client = new USBPcapClient(primary.filterId, captureExisting: false);
 
 client.DataRead += (sender, eventArgs) =>
 {
 
-    Console.WriteLine( $"DATA READ Device:'{new[] {Convert.ToByte(eventArgs.Header.device)}.ToHexString()}'" + $" in?:{eventArgs.Header.In} " + $"func:'{eventArgs.Header.function}' "
+    Console.WriteLine( $"DATA READ Device:'{eventArgs.Header.device}'" + $" in?:{eventArgs.Header.In} " + $"func:'{eventArgs.Header.function}' "
                      + $"len: {eventArgs.Data.Length} ");
 
     if (eventArgs.Data.Length > 100)
@@ -41,10 +41,14 @@ client.DataRead += (sender, eventArgs) =>
             $"DATA READ Device:'{eventArgs.Header.device}' in?:{eventArgs.Header.In} func:'{eventArgs.Header.function}' len: {eventArgs.Data.Length} data:{eventArgs.Data.ToHexString()}");
     }
 
-    if (eventArgs.Header.In && eventArgs.Data.Length > 0 && Utils.CompareBytes(
+    if (eventArgs.Data.Length > Marshal.SizeOf<>())
+    {
+
+    }
+
+    if (eventArgs.Header.In && eventArgs.Data.Length > 0 && Utils.ContainsTargetBytes(
             eventArgs.Data,
-            new byte[] { 0x02, 0x01, 0x00, 0x00 },
-            4))
+            new byte[] { 0x02, 0x01, 0x00, 0x00 }))
     {
         Console.WriteLine("CAPTURE!!!!");
     }
@@ -53,3 +57,5 @@ client.DataRead += (sender, eventArgs) =>
 client.start_capture();
 
 client.wait_for_exit_signal();
+
+
